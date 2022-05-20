4f8cabd814cc58324eeaefe68e5731bfb3c0e2a5
Tristyn <tristynstimpson@gmail.com>
2022-05-20 12:00:22 +0300

    Wallet connect api key

--- app/src/utils/WalletConnectUtil.ts
+++ app/src/utils/WalletConnectUtil.ts
@@ -1,17 +1,22 @@
-import WalletConnectClient from '@walletconnect/client'
+import WalletConnectClient from "@walletconnect/client";
 
-export let walletConnectClient: WalletConnectClient
+export let walletConnectClient: WalletConnectClient;
 
 export async function createWalletConnectClient() {
+  console.log(
+    process.env.NEXT_PUBLIC_PROJECT_ID,
+    process.env.NEXT_PUBLIC_RELAY_URL
+  );
   walletConnectClient = await WalletConnectClient.init({
     controller: true,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    relayUrl: process.env.NEXT_PUBLIC_RELAY_URL ?? 'wss://relay.walletconnect.com',
+    relayUrl:
+      process.env.NEXT_PUBLIC_RELAY_URL ?? "wss://relay.walletconnect.com",
     metadata: {
-      name: 'React Wallet',
-      description: 'React Wallet for WalletConnect',
-      url: 'https://walletconnect.com/',
-      icons: ['https://avatars.githubusercontent.com/u/37784886']
-    }
-  })
+      name: "React Wallet",
+      description: "React Wallet for WalletConnect",
+      url: "https://walletconnect.com/",
+      icons: ["https://avatars.githubusercontent.com/u/37784886"],
+    },
+  });
 }
