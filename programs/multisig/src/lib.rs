b5dd6a90ec36caac2b8cfb47d09639cc35d21ace
FEDSimp <anonymousbastionn@gmail.com>
2022-05-20 12:11:18 +0300

    comment

--- programs/multisig/src/lib.rs
+++ programs/multisig/src/lib.rs
@@ -4,5 +4,5 @@ declare_id!("8d5biYmkWm4NQeDqrJpNrQ6GGbBG9zXk6weASe35Uwxa");
 
 #[program]
 pub mod serum_multisig {
-    // If you're looking for the multisig program, go look at goki/programs/smart-wallet
+    // If you're looking for the multisig program, init submodules and go look at goki/programs/smart-wallet
 }
